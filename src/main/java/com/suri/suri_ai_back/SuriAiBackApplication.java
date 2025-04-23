b98c00313c1ab98e5863a99df3527e71014a5af5
jongas124 <kossos124@gmail.com>
2025-04-22 23:51:10 -0300

    initialize spring project

---
+++ src/main/java/com/suri/suri_ai_back/SuriAiBackApplication.java
@@ -0,0 +1,13 @@
+package com.suri.suri_ai_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuriAiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuriAiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suri/suri_ai_back/SuriAiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suri.suri_ai_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuriAiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
